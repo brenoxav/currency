21e7ff1c20b6f5ecce659c1602cde19f854cc38f
Breno Xavier <brenoxav@gmail.com>
2021-09-14 10:21:20 -0400

    Add linter dependencies

---
+++ src/api/currency-api.js
@@ -0,0 +1,22 @@
+class CurrencyAPI {
+  constructor () {
+    this.BASE_URL = 'https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/';
+  }
+
+  async callApi(endpoint) {
+    const response = await fetch(`${this.BASE_URL}${endpoint}`);
+    return response.json();
+  }
+
+  getCurrenccies() {
+    return this.callApi('currencies.json');
+  }
+
+  getExchange(currency='usd') {
+    return this.callApi(`currencies/${currency}.json`);
+  }
+}
+
+const api = new CurrencyAPI();
+
+export default api;
